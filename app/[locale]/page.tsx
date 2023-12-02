cadf7d9e3eccf3fbe3809dfe8338cf5cab1bea68
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-12-02 11:29:19 +0200

    Support MultiLanuage

--- app/[locale]/page.tsx
+++ app/[locale]/page.tsx
@@ -1,9 +1,11 @@
 import SwitchMode from "./switchMode"
+import {useTranslations} from 'next-intl';
 
 export default function Home() {
+  const t = useTranslations('Index');
   return (
     <main className="flex items-center justify-center h-screen flex-col">
-        <h1 className="dark:bg-slate-400"> How are you ther </h1>
+        <h1 className="dark:bg-slate-400"> {t('title')} </h1>
     </main>
   )
 }
--- push.sh
+++ push.sh
@@ -1,7 +1,3 @@
-git init
 git add *
-git commit -m "Add dark theme"
-git branch -M main
-git branch -M firstsprint
-git remote add origin git@github.com:13-Ronin/Frontend.git
-git push -u origin main
+git commit -m "$1"
+git push
