857448cbe7d2f5c7c05d595ed42fe249ca1d3a5c
mohamedahmed-cloud <modyyousef800@gmail.com>
2023-12-02 11:58:51 +0200

    provide a button to swith between language

--- middleware.ts
+++ middleware.ts
@@ -1,11 +1,13 @@
 import createMiddleware from 'next-intl/middleware';
- 
+import {locales, localePrefix} from './navigation';
+
 export default createMiddleware({
   // A list of all locales that are supported
-  locales: ['en', 'ar'],
  
   // Used when no locale matches
-  defaultLocale: 'en'
+  defaultLocale: 'en',
+  localePrefix,
+  locales
 });
  
 export const config = {
