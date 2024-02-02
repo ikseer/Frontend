70ba5ee08e39083327b4d6ff8a21e83db7e58048
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-02-02 02:00:45 +0000

    test

--- .lintstagedrc.js
+++ .lintstagedrc.js
@@ -4,7 +4,7 @@ const buildEslintCommand = (filenames) =>
   `next lint --fix --file ${filenames
     .map((f) => path.relative(process.cwd(), f))
     .join(' --file ')}`
- 
+
 module.exports = {
-  '*.{js,jsx,ts,tsx}': [buildEslintCommand],
+  '*.{ts,tsx}': [buildEslintCommand, "tsc-files --noEmit", "prettier -w"],
 }
