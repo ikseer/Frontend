68a8886e2409c87e09ff06d4d6528cbc48421e52
Mohamed Ahmed Yousef <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-02-03 18:43:45 +0000

    update

--- components/Tabs/Tabs.tsx
+++ components/Tabs/Tabs.tsx
@@ -1,22 +1,29 @@
+import React from 'react';
 // icons
-import { LuUser } from "react-icons/lu";
-import { LuSettings } from "react-icons/lu";
+import { LuUser } from 'react-icons/lu';
+import { LuSettings } from 'react-icons/lu';
 // payment icons didn't found in react icons,
 // icon found in lucide library but din't found in react-icons lib.
-import { MdPayment } from "react-icons/md";
-
-
+import { MdPayment } from 'react-icons/md';
 
 interface TabsType {
-  profileSetting: React.ReactNode
-  paymentSetting: React.ReactNode
-  securitySetting: React.ReactNode
+  profileSetting: React.ReactNode;
+  paymentSetting: React.ReactNode;
+  securitySetting: React.ReactNode;
 }
-export default function Tabs({ profileSetting, paymentSetting, securitySetting }: TabsType) {
+export default function Tabs({
+  profileSetting,
+  paymentSetting,
+  securitySetting,
+}: TabsType) {
   return (
     <>
       <div className="mt-10">
-        <nav className="flex space-x-2 justify-center" aria-label="Tabs" role="tablist">
+        <nav
+          className="flex space-x-2 justify-center"
+          aria-label="Tabs"
+          role="tablist"
+        >
           <button
             type="button"
             className=" hs-tab-active:font-semibold hs-tab-active:border-gray-200 hs-tab-active:text-gray-200 py-4 px-1 inline-flex items-center gap-x-2 border-b-2 border-transparent text-sm whitespace-nowrap text-gray-500 hover:text-gray-200 focus:outline-none focus:text-gray-200 disabled:opacity-50 disabled:pointer-events-none dark:text-gray-400 dark:hover:text-gray-100 active"
@@ -24,7 +31,6 @@ export default function Tabs({ profileSetting, paymentSetting, securitySetting }
             data-hs-tab="#tabs-with-icons-1"
             aria-controls="tabs-with-icons-1"
             role="tab"
-
           >
             {<LuUser />}
             Basic Settings
