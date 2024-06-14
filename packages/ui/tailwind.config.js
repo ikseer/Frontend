5cb580540eaec8323d67abf795983a537a5aa320
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-06-14 17:53:47 +0300

    refactor login page

--- packages/ui/tailwind.config.js
+++ packages/ui/tailwind.config.js
@@ -4,9 +4,7 @@ module.exports = {
 	content: [
 		"./pages/**/*.{ts,tsx}",
 		"./components/**/*.{ts,tsx}",
-		"./app/**/*.{ts,tsx}",
 		"./src/**/*.{ts,tsx}",
-		"../../packages/ui/src/**/*.{ts,tsx}",
 	],
 	prefix: "",
 	theme: {
