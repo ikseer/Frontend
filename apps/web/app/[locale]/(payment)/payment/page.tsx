c36aee77fbc410082a59d9c67b75f79e3750c70a
mohamedahmed-cloud <81932663+mohamedahmed-cloud@users.noreply.github.com>
2024-07-05 02:07:25 +0300

    fix payment page

--- apps/web/app/[locale]/(payment)/payment/page.tsx
+++ apps/web/app/[locale]/(payment)/payment/page.tsx
@@ -1,7 +1,9 @@
 "use client";
+import { ErrorMsg } from "@/components/error-msg";
 import Radio from "@/components/radio";
 import Spinner from "@/components/spinner";
 import { useZodForm } from "@/lib/use-zod-form";
+import { useRouter } from "@/navigation";
 import useCreateOrder, {
 	useCreatePaymobOrderId,
 	useGetPaymobToken,
@@ -9,24 +11,34 @@ import useCreateOrder, {
 import { UserIdCookie } from "@ikseer/lib/cookies.client";
 import { Button } from "@ikseer/ui/components/ui/button";
 import { FormInput } from "@ikseer/ui/components/ui/input";
+import { Label } from "@ikseer/ui/components/ui/label";
+import { useState } from "react";
 import { FormProvider } from "react-hook-form";
 import { z } from "zod";
 
 const schema = z.object({
-	owner: z.string(),
-	location: z.string(),
+	first_name: z.string(),
+	last_name: z.string(),
+	country: z.string(),
+	city: z.string(),
+	street: z.string(),
 	phone: z.string(),
+	email: z.string().email(),
 	payment: z.string(),
+	zipcode: z.string(),
 });
 
 export default function Payment() {
+	const router = useRouter();
+	const [payNow, setPayNow] = useState(true);
 	const getPaymobToken = useGetPaymobToken((data) => {
-		console.log(data);
-		window.location.href = `https://accept.paymob.com/api/acceptance/iframes/831751?payment_token=${data.token}`;
+		window.open(
+			`https://accept.paymob.com/api/acceptance/iframes/831751?payment_token=${data.token}`,
+			"_blank",
+		);
 	});
 
 	const createPaymobOrderId = useCreatePaymobOrderId((data) => {
-		console.log(data, "useCreatePaymobOrderId");
 		getPaymobToken.mutate({
 			amountInCents: data.amount_cents,
 			orderId: data.paymob_order_id,
@@ -34,8 +46,8 @@ export default function Payment() {
 	});
 
 	const createOrder = useCreateOrder((order) => {
-		console.log(order, "order");
-		createPaymobOrderId.mutate(order);
+		if (payNow) createPaymobOrderId.mutate(order);
+		else router.push("/orders");
 	});
 
 	const form = useZodForm({
@@ -43,31 +55,60 @@ export default function Payment() {
 	});
 
 	const userId = UserIdCookie.get() as string;
-
 	return (
 		<FormProvider {...form}>
 			<form
 				className="page-container space-y-6"
 				onSubmit={form.handleSubmit((data) => {
+					if (data.payment === "payLater") {
+						setPayNow(false);
+					}
 					createOrder.mutate({ ...data, user: userId });
 				})}
 			>
-				<h1 className="text-2xl font-bold">Checkout</h1>
-				<FormInput name="owner" label="Owner" className="rounded-md" />
-				<FormInput name="location" label="Location" className="rounded-md" />
+				<h1 className="text-2xl font-bold">Add a new address</h1>
+				<div className="gap-x-4 flex items-center">
+					<FormInput
+						name="first_name"
+						label="First name"
+						className="rounded-md"
+					/>
+					<FormInput
+						name="last_name"
+						label="Last name"
+						className="rounded-md"
+					/>
+				</div>
+				<FormInput name="country" label="Country" className="rounded-md" />
+				<FormInput name="city" label="City" className="rounded-md" />
+				<FormInput name="street" label="Street" className="rounded-md" />
+				<FormInput name="zipcode" label="Zipcode" className="rounded-md" />
 				<FormInput name="phone" label="Phone" className="rounded-md" />
-				<section className="gap-x-6 flex">
-					<div className="gap-x-2 flex items-center">
-						<Radio name="payment" value="paymob" />
-						<label htmlFor="paymob">Paymob</label>
-					</div>
-					<div className="gap-x-2 flex items-center">
-						<Radio name="payment" value="noPayment" />
-						<label htmlFor="noPayment">Pay when order reach</label>
-					</div>
+				<FormInput name="email" label="Email" className="rounded-md" />
+				<section className="space-y-2">
+					<section className="gap-x-6 flex">
+						<div className="gap-x-2 flex items-center">
+							<Radio name="payment" value="payNow" />
+							<Label htmlFor="payNow">Pay now</Label>
+						</div>
+						<div className="gap-x-2 flex items-center">
+							<Radio name="payment" value="payLater" />
+							<Label htmlFor="payLater">Pay later</Label>
+						</div>
+					</section>
+					{form.formState.errors.payment?.message && (
+						<ErrorMsg>{form.formState.errors.payment?.message}</ErrorMsg>
+					)}
 				</section>
 				<Button variant="submit" disabled={createOrder.isPending}>
-					{createOrder.isPending ? <Spinner /> : "Create Order"}
+					{createOrder.isPending ? (
+						<>
+							{" "}
+							Create Order &nbsp; <Spinner />
+						</>
+					) : (
+						"Create Order"
+					)}
 				</Button>
 			</form>
 		</FormProvider>
